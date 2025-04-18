dc4dd6c0088db03847df161547005d15ab15d59b
gabei <gabe1792@gmail.com>
2025-04-18 13:54:47 -0500

    Chore: Configure Typescript and begin implementation. Update dependencies with @types. Rewrite index.js with new types

---
+++ api/src/index.ts
@@ -0,0 +1,28 @@
+import express, {Request, Response} from "express";
+const app = express();
+const cors = require('cors');
+const corsOptions = {
+  origin: [],
+  successStatus: 200
+}
+
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+app.post('/contact', async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    res.status(200).json({message: "login success!", data: req.body});
+  }
+  catch(e) {
+    console.log(e);
+    res.status(400).json({message: "problem logging in", data: e});
+  }
+})
+
+
+app.listen(3000, ()=> {
+  console.log("Server is listening on port 3000...");
+})
